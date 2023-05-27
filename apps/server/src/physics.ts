dafeb84f38817daa87313c6283f12b7f79e1f8fe
Ivan Bilicki <ivanbili@gmail.com>
2023-05-27 15:38:31 -0400

    first attempt at physics

--- apps/server/src/physics.ts
+++ apps/server/src/physics.ts
@@ -6,7 +6,18 @@ export interface IComputeNextPhysicsBodyPosition {
 }
 
 // FOR: IVAN
-export const computeNextPosition: IComputeNextPhysicsBodyPosition = (deltaT, body) => {
-  return { x: 0, y: 0 };
+export const computeNextPosition: IComputeNextPhysicsBodyPosition = (deltaT: number, body: PhysicsBody) => {
+
+  let time_factor: number = (deltaT / body.lastDeltaT);
+  let delta_x: number = (body.position.x - body.lastPosition.x + body.acceleration.x) * time_factor;
+  let delta_y: number = (body.position.y - body.lastPosition.y + body.acceleration.y) * time_factor;
+  let new_x: number = body.lastPosition.x + delta_x;
+  let new_y: number = body.lastPosition.y + delta_y;
+  body.lastDeltaT = deltaT;
+  body.lastPosition = body.position;
+  body.position.x = new_x;
+  body.position.y = new_y;
+  // why do we need to return anything? let's just update it here
+  return { x: new_x, y: new_y };
 };
 
