87227b1edc7aa870c7cb99c171d94f721bc6cf7b
Victor Nguyen <vnguyen@Victors-MacBook-Pro.local>
2023-05-27 16:18:27 -0400

    lobby update

--- apps/client/src/views/Welcome.vue
+++ apps/client/src/views/Welcome.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-  import ButtonBasic from './ButtonBasic.vue';
+  import ButtonBasic from '../components/ButtonBasic.vue';
 </script>
 
 <template>
   <div class="flex flex-col justify-around items-center w-full h-screen bg-gradient-to-r from-blue-500 via-red-500 to-yellow-500 background-animate">
    <div class="flex flex-col items-center"> 
-    <p class="text-lg font-bold tracking-tight text-indigo-700">Pooping in public made easier</p>
-    <h1 class="text-6xl font-bold tracking-wide text-black"> Game of Poops </h1>
+    <p class="text-lg font-bold tracking-tight text-indigo-900">Pooping in public made easier</p>
+    <h1 class=" uppercase text-6xl font-bold tracking-tight text-black"> Game of Poops </h1>
    </div>
     <router-link
     to="/lobby"
