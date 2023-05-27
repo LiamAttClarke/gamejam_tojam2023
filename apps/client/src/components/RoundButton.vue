2e61231595f7d3b908eb136968fa73ca92d2b67a
Artemy <artemym@gmail.com>
2023-05-26 22:00:11 -0400

    basic character select

---
+++ apps/client/src/components/RoundButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button type="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  border-radius: 50px;
+  cursor: pointer;
+  border: 1px solid black;
+}
+</style>
