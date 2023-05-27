aef26774d134ec0b7dab85678c5eb4ac0adec919
Victor Nguyen <vnguyen@Victors-MacBook-Pro.local>
2023-05-27 19:20:42 -0400

    added assets

--- apps/client/src/views/Welcome.vue
+++ apps/client/src/views/Welcome.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="flex flex-col justify-around items-center w-full h-screen bg-gradient-to-r from-blue-500 via-red-500 to-yellow-500 background-animate">
+  <div class="flex flex-col justify-center items-center w-full h-screen bg-gradient-to-r from-blue-500 via-red-500 to-yellow-500 background-animate">
    <div class="flex flex-col items-center"> 
     <p class="text-lg font-bold tracking-tight text-indigo-900">Pooping in public made easier</p>
     <h1 class=" uppercase text-6xl font-bold tracking-tight text-black"> Game of Poops </h1>
