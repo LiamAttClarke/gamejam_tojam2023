128362b319a387275b93668b4c4f8b0d3d8593f1
Victor <victor@sentryhealth.ca>
2023-05-27 15:06:47 -0400

    Updated game lobby

---
+++ apps/client/src/views/GameLobby.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="text-lg">Game Lobby</h1>
+
+    <button>Create New Game</button>
+    <input type="text" id="roomInput" v-model:="roomId" placeholder="Enter Room ID">
+    <button>Join  Game</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const roomId = ref('');
+function createGame() {
+
+}
+function joinGame() {
+  if (roomId.value!== '') {
+    console.log(roomId.value);
+  }
+}
+</script>
