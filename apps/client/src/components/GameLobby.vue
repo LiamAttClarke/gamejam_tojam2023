9df84ed77248f1c6fc06e4b7f6f9d315bd95d661
Victor Nguyen <vnguyen@Victors-MacBook-Pro.local>
2023-05-26 21:58:41 -0400

    chore: tailwind update

--- apps/client/src/components/GameLobby.vue
+++ apps/client/src/components/GameLobby.vue
@@ -1,6 +1,20 @@
 <template>
-  <div><h1 class="font-medium text-red">Game Lobby</h1>
-  <div class="text-">Test</div>
+  <div>
+    <h1 class="text-lg">Game Lobby</h1>
+    <h2>Current Games</h2>
+    <ul>
+      <li>
+        <button>XXXX</button>
+      </li>
+      <li>
+        <button>XXXX</button>
+      </li>
+      <li>
+        <button>XXXX</button>
+      </li>
+    </ul>
+  
+  <button>Create New Game</button>
   </div>
   
 </template>
