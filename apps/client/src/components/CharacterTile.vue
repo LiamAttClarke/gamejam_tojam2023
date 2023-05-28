3170d8db29389c361bed03eb93704807b8b445d1
Victor Nguyen <vnguyen@Victors-MacBook-Pro.local>
2023-05-28 16:22:44 -0400

    ui update

--- apps/client/src/components/CharacterTile.vue
+++ apps/client/src/components/CharacterTile.vue
@@ -27,7 +27,7 @@ const props = defineProps({
       @change="$emit('change', character)"
       name="char"
     />
-    <div class="tile">
+    <div class="tile bg-white">
       <img :src="`/assets/avatars/${character}.png`" alt="character" width="100" height="100" />
     </div>
   </label>
