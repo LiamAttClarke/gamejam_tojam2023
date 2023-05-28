e53c15938da9c5e3e87a7a718b28b0ca46b94398
Alex Alksne <alex.alksne@gmail.com>
2023-05-27 20:45:55 -0400

    add move listener

--- apps/server/src/events/rooms/createRoom.ts
+++ apps/server/src/events/rooms/createRoom.ts
@@ -1,5 +1,7 @@
 import { Socket } from "socket.io";
 import { RoomManager } from "../../RoomManager";
+import constants from "../../../../shared/constants";
+import onDirectionChange from "../onDirectionChange";
 
 export default (socket: Socket) => () => {
   console.log(`create room, socket id: ${socket.id}`);
@@ -7,4 +9,6 @@ export default (socket: Socket) => () => {
   const roomManager = RoomManager.getInstance();
 
   roomManager.createRoom(socket);
+
+  socket.on(constants.MSG_TYPES.DIRECTION_CHANGE, onDirectionChange(socket));
 }
